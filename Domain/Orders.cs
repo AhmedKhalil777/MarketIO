11995099a40bb356069bbbbf6922d51355659227
MohamedAshraf004 <lionelmido004@gmail.com>
2020-04-25 19:18:15 +0200

    update order class

--- Domain/Orders.cs
+++ Domain/Orders.cs
@@ -25,25 +25,11 @@ namespace MarketIO.MVC.Domain
 
 
 
-        public float Total_Price()
-        {
-            float TotalPrice = 0;
-            Order_Details.ForEach(Order => { TotalPrice += Order.Current_Price * Order.Amount; });
-            return TotalPrice;
-        }
-    }
-
-
-
-    public enum OrderStatus
-    {
-        // If the Market accept to make the quote 
-        Accepted = 0,
-        // Explain if their are some issues 
-        Issue = 1,
-        // Explain if the order is recieved to customer
-        Recieved = 2,
-        // Explain if the Order is out from the store and shipped to the customer
-        Shipped =4 
+        //public float Total_Price()
+        //{
+        //    float TotalPrice = 0;
+        //    Order_Details.ForEach(Order => { TotalPrice += Order.Current_Price * Order.Amount; });
+        //    return TotalPrice;
+        //}
     }
 }
