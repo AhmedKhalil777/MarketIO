cc3e976996693234c61e9c75351a1a7b2fddc6cb
AhmedKhalil777 <progeng_Ahmed_Khalil@outlook.com>
2020-04-27 04:01:12 +0200

    Adding Login for Admin

---
+++ Controllers/V1/MVC/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketIO.MVC.Contracts.V1;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketIO.MVC.Controllers.V1.MVC
+{
+    public class AccountController : Controller
+    {
+        [HttpGet(MVCRoutes.Admin.Base)]
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
